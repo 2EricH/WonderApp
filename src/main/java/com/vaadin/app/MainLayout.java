0a57ea636a6e5a2440d08edf47318770d4c46e74
2EricH <eric.hedlund.2017@live.rhul.ac.uk>
2019-05-04 00:04:28 +0100

    New split views and embeded vertical layouts

---
+++ src/main/java/com/vaadin/app/MainLayout.java
@@ -0,0 +1,42 @@
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.router.RouterLayout;
+
+@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=no")
+
+public class MainLayout extends VerticalLayout implements RouterLayout {
+private static final long serialVersionUID = 4L;
+
+public MainLayout() {
+	this.primaryLayout();
+	this.secondaryLayout();
+}
+
+    public VerticalLayout primaryLayout() {
+		VerticalLayout layout = new VerticalLayout();
+		
+		Label firstLabel = new Label("Circular Design View");
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        
+        layout.add(firstLabel, button);
+        return(layout);
+    }
+    
+    public VerticalLayout secondaryLayout() {
+		VerticalLayout layout = new VerticalLayout();
+		
+		Label secondLabel = new Label("Toolbar");
+        Button button = new Button("Click me",
+                event -> Notification.show("Clicked!"));
+        
+        layout.add(secondLabel, button);
+        return(layout);
+    }
+}
--- src/main/java/com/vaadin/app/MainView.java
+++ src/main/java/com/vaadin/app/MainView.java
@@ -3,27 +3,29 @@ package com.vaadin.app;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
-/**
- * The main view contains a button and a click listener.
- */
+@SuppressWarnings("serial")
 @Route("")
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
-@Tag("div")
-@HtmlImport("C:/GIT/Sharon-App/theming.html")
-//@Theme(value = Lumo.class, variant = Lumo.DARK)
-public class MainView extends VerticalLayout {
-	private static final long serialVersionUID = 4L;
+@Theme(value = Lumo.class, variant = Lumo.DARK)
 
+public class MainView extends SplitLayout {
+	
 	public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+		MainLayout fillview = new MainLayout();
+		
+		setPrimaryStyle("minWidth", "400px");
+		setPrimaryStyle("maxWidth", "600px");
+	
+        addToPrimary(fillview.primaryLayout());
+        addToSecondary(fillview.secondaryLayout());
     }
 }
